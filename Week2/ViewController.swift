5cdd2ec4247473496f3cd62bb21613b00cbba424
PRIYAM PATEL <patelpriyam79@gmail.com>
2024-01-15 18:12:39 -0500

    Initial Commit

---
+++ Week2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week2
+//
+//  Created by PRIYAM PATEL on 15/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
